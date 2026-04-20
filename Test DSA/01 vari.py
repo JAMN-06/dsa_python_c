618037691d1613842c337eaafe518153b2d134af
John <mwangiabraham02@gmail.com>
2026-04-20 12:14:57 +0300

    Loops

--- Test DSA/01 vari.py
+++ Test DSA/01 vari.py
@@ -1,1 +1,18 @@
 print("Hello World!")
+name = "John"
+name2 = "John"
+num = 25
+balance = 159.258
+flag = True
+
+print(id(name))    
+print(id(name2))
+  
+# comment
+"""This is a multi-line comment
+that spans multiple lines."""
+
+print(type(num))
+print(type(num))
+print(type(balance))
+print(type(flag))
