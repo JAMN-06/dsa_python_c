ef2a8a6a04106254c0d33404417bcf1f0ee651a7
John <mwangiabraham02@gmail.com>
2026-05-11 10:58:00 +0300

    Recursion

---
+++ Test DSA/recursion.py
@@ -0,0 +1,36 @@
+#import random
+
+#def tail_recursion(n:{__It__, __sub__}):
+    #if n < 1:
+       #return n # base case
+    
+    #print(n)
+    #return tail_recursion(n-1) # recursive call
+
+#tail_recursion(5)
+
+#def a(n:{__gt__}):
+    #if n > 0:
+       #print(f"Inside a: {n}")
+        #return b(n-1)
+    
+    #if n > 0:
+        #return(n)
+    #else:
+        #print(f"Inside a: {n}")
+        #return b(n-1)
+    
+
+#def b(n:{__gt__}):
+    #if n > 0:
+        #print(f"Inside b: {n}")
+        #return a(n-1)
+    
+#a(5)
+
+def head_recursion(n:{__It__, __sub__}):
+    if n < 1:
+        return n # base case
+    
+    head_recursion(n-1) # recursive call
+    print(n)
